eaf41664938bde2d9efb43203ff5b44e4d07acf3
Steven Kalt <kalt.steven@gmail.com>
2019-01-01 14:03:17 -0500

    more progress, I guess

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.scss'
+
+function snow(w, h) { // must be 1:1 for px
+  const {random, round} = Math;
+  const flake = (x, y) => `<circle cx="${x}" cy="${y}" r="10" />`
+  return Array(round(w * h / 1000))
+    .fill(0)
+    .map(()=> [random() * w, random() * h])
+    .map(([x, y]) => flake(x,y))
+    .join('\n')
+}
+
+// make a snow svg <template>
+// copy it to each cloud-layer ?
