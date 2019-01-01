8fb22be30b9ea5412c32f465907836ea050f8589
Steven Kalt <kalt.steven@gmail.com>
2019-01-01 18:58:28 -0500

    using clip for the snow outline

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import './style.scss'
+const qsa = s => [...document.querySelectorAll(s)]
 
-function snow(w, h) { // must be 1:1 for px
+function snow({n, r=10, w, h}) { // must be 1:1 for px
   const {random, round} = Math;
-  const flake = (x, y) => `<circle cx="${x}" cy="${y}" r="10" />`
-  return Array(round(w * h / 1000))
+  const flake = (x, y, r) => `<circle cx="${x}" cy="${y}" r="${r}" />`
+  return Array(n)
     .fill(0)
     .map(()=> [random() * w, random() * h])
-    .map(([x, y]) => flake(x,y))
+    .map(([x, y]) => flake(x,y,r))
     .join('\n')
 }
 
+snow_2.innerHTML= snow({n: 3000, r:.2, w: 300, h: 300})
+Object.assign(window, {snow, qsa})
+// window.snow = snow(1000, 1000)
 // make a snow svg <template>
 // copy it to each cloud-layer ?
