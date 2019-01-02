b7ff8dccea6f17c4e799cc6d7eb1466f04b4682f
Steven Kalt <kalt.steven@gmail.com>
2019-01-01 20:52:57 -0500

    done

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
 import './style.scss'
-const qsa = s => [...document.querySelectorAll(s)]
 
-function snow({n, r=10, w, h, minY=0, minX=0}) { // must be 1:1 for px
-  const {random, round} = Math;
+function snow(id, {n, r=10, w, h, minY=0, minX=0}) { // must be 1:1 for px
+  const {random} = Math;
   const flake = (x, y, r) => `<circle cx="${x}" cy="${y}" r="${r}" />`
-  return Array(n)
+  document.getElementById(`snow_${id}`).innerHTML =  Array(n)
     .fill(0)
     .map(()=> [random() *  w + minX, random() * h + minY])
     .map(([x, y]) => flake(x,y,r))
     .join('\n')
 }
 
-snow_2.innerHTML= snow({n: 3000, r:.4, w: 300, h: 300})
-snow_1.innerHTML= snow({n: 3000, r:.53, w: 300, h: 300})
-snow_3.innerHTML= snow({n: 1000, r:.5, w: 600, h: 600, minX: -200,})
-snow_4.innerHTML= snow({n: 1000, r:4, w: 2000, h: 2000})
-
-Object.assign(window, {snow, qsa})
-// window.snow = snow(1000, 1000)
-// make a snow svg <template>
-// copy it to each cloud-layer ?
+[
+  {n: 3000, r:.5, w: 500, h: 300, minX: -100},
+  {n: 3000, r:.35, w: 300, h: 300},
+  {n: 1000, r:.5, w: 600, h: 600, minX: -200,},
+  {n: 1000, r:4, w: 2000, h: 2000},
+  {n: 1000, r:3.5, w: 2000, h: 2000},
+].forEach((opts, i) => snow(i+1, opts))
