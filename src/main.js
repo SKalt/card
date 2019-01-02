66cc9ac1ce333704d9eaaef86619be55fdf45564
Steven Kalt <kalt.steven@gmail.com>
2019-01-02 00:25:16 -0500

    really done

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ import {
 
 function snow(id, {n, r=10, w, h, minY=0, minX=0}) { // must be 1:1 for px
   const {random} = Math;
-  const flake = (x, y, r) => `<circle cx="${x}" cy="${y}" r="${r}" /></circle>`
+  const flake = (x, y, r) => `<circle cx="${x}" cy="${y}" r="${r}" />s`
   document.getElementById(`snow_${id}`).innerHTML =  Array(n)
     .fill(0)
     .map(()=> [random() *  w + minX, random() * h + minY])
     .map(([x, y]) => flake(x,y,r))
     .join('\n')
 }
-window.onload = function(){
 
+window.onload = function(){
   [
-    {n: 3000, r:.5, w: 500, h: 300, minX: -100},
+    {n: 3000, r:.35, w: 500, h: 300, minX: -100},
     {n: 3000, r:.35, w: 300, h: 300},
     {n: 1000, r:.5, w: 600, h: 600, minX: -200,},
     {n: 1000, r:4, w: 2000, h: 2000},
@@ -25,7 +25,7 @@ window.onload = function(){
   const msg = document.getElementById('msg')
   if (location.search)  msg.innerText = decompress(location.search.slice(1))
   msg.addEventListener('keypress', () =>{
-    const path = `${location.protocol}//${location.origin}?${compress(msg.innerText)}`
+    const path = `${location.origin}?${compress(msg.innerText)}`
     history.pushState({path}, '', path)
   })
 }
