682716ea4b634728bef11521ae59804fdf2b3652
Steven Kalt <kalt.steven@gmail.com>
2021-12-30 21:19:24 -0500

    chore: upgrade dependencies, use pnpm

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import "./style.scss";
 import {
   compressToEncodedURIComponent as compress,
-  decompressFromEncodedURIComponent as decompress
+  decompressFromEncodedURIComponent as decompress,
 } from "lz-string";
 
 function snow(id, { n, r = 10, w, h, minY = 0, minX = 0 }) {
@@ -20,7 +19,7 @@ function snow(id, { n, r = 10, w, h, minY = 0, minX = 0 }) {
   { n: 3000, r: 0.35, w: 300, h: 300 },
   { n: 1000, r: 0.5, w: 600, h: 600, minX: -200 },
   { n: 1000, r: 4, w: 2000, h: 2000 },
-  { n: 1000, r: 3.5, w: 2000, h: 2000 }
+  { n: 1000, r: 3.5, w: 2000, h: 2000 },
 ].forEach((opts, i) => snow(i + 1, opts));
 const msg = document.getElementById("msg");
 if (location.search.slice(1)) {
